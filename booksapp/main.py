a68387c6004c51187a5a0a5554bc8d932936f562
d.mischenko <d.mischenko@digital-sector.ru>
2024-08-28 13:23:54 +0300

    Добавлен Alembic

--- booksapp/main.py
+++ booksapp/main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 from sqlalchemy.testing.plugin.plugin_base import logging
 
 from core.config import settings
-from core.models import db_helper, Base
 from fastapi import FastAPI
 import logging
 from api_v1 import router as router_v1
@@ -13,10 +12,6 @@ logger = logging.getLogger()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.debug(f'{settings.DATABASE_URL()}')
-    async with db_helper.engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 app = FastAPI(lifespan=lifespan)
