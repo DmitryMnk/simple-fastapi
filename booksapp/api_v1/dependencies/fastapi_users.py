5c48a9b4fb475c6c065aba91805ae1f5c0d7493c
d.mischenko <d.mischenko@digital-sector.ru>
2024-09-13 08:01:09 +0300

    создан fastapi_users routers helper

---
+++ booksapp/api_v1/dependencies/fastapi_users.py
@@ -0,0 +1,13 @@
+
+
+from fastapi_users import FastAPIUsers
+
+from core.authentication.depebdencies import get_user_manager
+from core.models import User
+from core.types.user_id import UserIdType
+from .backend import auth_backend
+
+fastapi_users = FastAPIUsers[User, UserIdType](
+    get_user_manager,
+    [auth_backend]
+)
